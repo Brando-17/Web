2e8621d0275b672a613e241c9ec9ec366c3757ee
Brando-17 <155272252+Brando-17@users.noreply.github.com>
2024-04-23 18:05:11 -0400

    Add files via upload

---
+++ Web.js
@@ -0,0 +1,30 @@
+- What is HTTP?
+Hypertext Transfer Protocol
+
+- What is a URL?
+Stands for Uniform Resource Locator and is the address of a resouce on the internet. 
+
+- What is DNS?
+Stands for Domain Name system. Changes domain names into IP addresses.
+
+- What is a query string?
+It is the end of the url and provides extra information.
+
+- What are two HTTP verbs and how are they different?
+Post - sends data to the server
+Get- gets some data from the server
+
+- What is an HTTP request?
+A request that is from a client that is requested to a server
+
+- What is an HTTP response?
+The response from a server to a client
+
+- What is an HTTP header? Give a couple examples of request and response headers you have seen.
+Http header is additional info about the request
+Request header-Accept, Cookie, Cache Control,Language,Date
+Response header-Last Modified, Cache Control, Content Type,Date,Time
+
+- What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+The name is changed into a IP address and then a request is made to that IP. Depending on if the request was successful it will load or you will get a error of some sort. Then the DOM will be made for that HTML.
+
